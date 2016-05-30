978e7c49e0e5c36ad73cf73ad8e421f1f45d5079
Valeria Lepina <divergence082@gmail.com>
2016-05-30 16:33:55 +0300

    kafka

--- kafka/scripts/kafka-run.sh
+++ kafka/scripts/kafka-run.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-trap "${KAFKA_HOME}/bin/kafka-server-stop.sh; echo 'Kafka stopped.'; ${KAFKA_HOME}/bin/zookeeper-server-stop.sh; echo 'Zookeeper stopped.'; exit" SIGHUP SIGINT SIGTERM
+trap "${KAFKA_HOME}/bin/kafka-server-stop.sh; echo 'Kafka stopped.'; exit" SIGHUP SIGINT SIGTERM
 
-exec ${KAFKA_HOME}/bin/zookeeper-server-start.sh /kafka/config/zookeeper.properties &
 exec ${KAFKA_HOME}/bin/kafka-server-start.sh /kafka/config/server.properties
